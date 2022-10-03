601e25fa6697def9a24aa4b713596b5bf893ffa9
s827 <dskumar827@gmail.com>
2022-10-03 19:54:11 +0530

    moved arrow function topic in a folder

---
+++ arrowFunctions/arrow.js
@@ -0,0 +1,11 @@
+const doubler = (item) => item * 2;
+console.log(doubler(4));
+
+const tripler = item => item * 3;
+console.log(tripler(4));
+
+const multiplier = (item1, item2) => item1 * item2;
+console.log(multiplier(12, 10));
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat([1, 2],[3, 4, 5]));
